c7d1ffc3d37ea38ad98997037ecc4b357ebcd104
Elise Muellerleile <eliamue@live.com>
2021-09-08 19:27:21 -0500

    API fetch fixed to include parent elements, list now displays

--- src/services/ZeldaApi.js
+++ src/services/ZeldaApi.js
@@ -1,8 +1,8 @@
 export const fetchCreatures = () => {
   return fetch('https://botw-compendium.herokuapp.com/api/v2/category/creatures')
-    .then(res => res.json())
-    .then(json => 
-      json.results.map(creature => ({
+    .then((res) => res.json())
+    .then((json) => 
+      json.data.non_food.map((creature) => ({
         id: creature.id,
         name: creature.name,
         description: creature.description,
